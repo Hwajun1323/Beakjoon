f674d6cef7d8a6d3bcc3e633c693c151a0b73018
Hwajun Lee <49684257+Hwajun1323@users.noreply.github.com>
2023-01-30 13:28:29 +0100

    Create Climbing_the_Leaderboard.java

---
+++ week3/Climbing_the_Leaderboard.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+        List<Integer> ranks = ranked.stream().distinct().collect(Collectors.toList());
+        int rank = 0;
+        for (int i = player.size() - 1; i >= 0; i--) {
+            for (int j = rank; j < ranks.size(); j++) {
+                if (player.get(i) >= ranks.get(j)) {
+                    player.set(i, rank + 1);
+                    break;
+                }
+                rank++;
+            }
+            if (rank == ranks.size()) player.set(i, rank + 1);
+        }
+        return player;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int rankedCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ranked = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int playerCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> player = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.climbingLeaderboard(ranked, player);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
